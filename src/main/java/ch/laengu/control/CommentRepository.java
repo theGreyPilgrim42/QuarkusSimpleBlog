ac1a1838591ea536b7bfbb6df010095893ca757b
leonardo.lang <leonardo.lang@hftm.ch>
2024-03-08 20:50:04 +0100

    feat(control): Add Author and Comment repositories

---
+++ src/main/java/ch/laengu/control/CommentRepository.java
@@ -0,0 +1,9 @@
+package ch.laengu.control;
+
+import ch.laengu.entity.Comment;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CommentRepository implements PanacheRepository<Comment> {
+}
